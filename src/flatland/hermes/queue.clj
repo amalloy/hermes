f0e9f7c3238c5b653c2be2db6ba838d9bc63168b
Alan Malloy <alan@malloys.org>
2013-02-26 18:11:23 -0800

    Add some queue/buffer utils

---
+++ src/flatland/hermes/queue.clj
@@ -0,0 +1,27 @@
+(ns flatland.hermes.queue)
+
+(defn pop-while [pred q]
+  (cond (empty? q) q
+        (pred (peek q)) (recur pred (pop q))
+        :else q))
+
+(defn message-buffer []
+  (atom (delay clojure.lang.PersistentQueue/EMPTY)))
+
+(defn add-message [buffer message pred]
+  (swap! buffer
+         (fn [queue]
+           (let [cleaned-queue @queue]
+             (delay (pop-while pred (conj cleaned-queue message)))))))
+
+(defn current-messages [buffer]
+  @@buffer)
+
+(defn numbered-message-buffer []
+  (message-buffer))
+
+(defn add-numbered-message [buffer message current-num min-to-keep]
+  (add-message buffer [current-num message] #(< (first %) min-to-keep)))
+
+(defn current-numbered-messages [buffer]
+  (map second (current-messages buffer)))
