ccf49df26ea70f701dfa61e7c2d8bf8c69e64db8
Alan Malloy <alan@malloys.org>
2013-03-04 16:36:31 -0800

    Release a non-snapshot

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.flatland/hermes "0.1.0-SNAPSHOT"
+(defproject org.flatland/hermes "0.1.0"
   :description "Push notifications for your browser."
   :url "http://github.com/flatland/hermes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [aleph "0.3.0-SNAPSHOT"]
+                 [aleph "0.3.0-beta14"]
                  [compojure "1.1.1"]
                  [ring-middleware-format "0.2.4" :exclusions [ring]]]
   :main flatland.hermes)
