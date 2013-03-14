d332e7a0bfbcd81be0490848ab702a24add83a3e
Alan Malloy <alan@malloys.org>
2013-03-14 12:40:16 -0700

    Provide a way to clear old messages when reading, not just writing

--- src/flatland/hermes/queue.clj
+++ src/flatland/hermes/queue.clj
@@ -8,11 +8,17 @@
 (defn message-buffer []
   (atom (delay clojure.lang.PersistentQueue/EMPTY)))
 
+(defn add-messages [buffer messages pred]
+  @(swap! buffer
+          (fn [queue]
+            (let [cleaned-queue @queue]
+              (delay (pop-while pred (into cleaned-queue messages)))))))
+
 (defn add-message [buffer message pred]
-  (swap! buffer
-         (fn [queue]
-           (let [cleaned-queue @queue]
-             (delay (pop-while pred (conj cleaned-queue message)))))))
+  (add-messages buffer [message] pred))
+
+(defn update-buffer [buffer pred]
+  (add-messages buffer [] pred))
 
 (defn current-messages [buffer]
   @@buffer)
@@ -20,11 +26,20 @@
 (defn numbered-message-buffer []
   (message-buffer))
 
+(defn- keep-pred [min-to-keep]
+  #(< (first %) min-to-keep))
+
 (defn add-numbered-message [buffer message current-num min-to-keep]
-  (add-message buffer [current-num message] #(< (first %) min-to-keep)))
+  (add-message buffer [current-num message] (keep-pred min-to-keep)))
 
-(defn messages-with-numbers [buffer]
-  (current-messages buffer))
+(defn messages-with-numbers
+  ([buffer]
+     (current-messages buffer))
+  ([buffer min-to-keep]
+     (update-buffer buffer (keep-pred min-to-keep))))
 
-(defn messages-without-numbers [buffer]
-  (map second (current-messages buffer)))
+(defn messages-without-numbers
+  ([buffer]
+     (map second (messages-with-numbers buffer)))
+  ([buffer min-to-keep]
+     (map second (messages-with-numbers buffer (keep-pred min-to-keep)))))
