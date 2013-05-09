4a7344b032cd2575a99318793a66c9e5624bcfac
Alan Malloy <alan@malloys.org>
2013-05-08 19:07:00 -0700

    0.1.7

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.flatland/hermes "0.1.7-SNAPSHOT"
+(defproject org.flatland/hermes "0.1.7"
   :description "Push notifications for your browser."
   :url "http://github.com/flatland/hermes"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.macro "0.1.2"]
-                 [aleph "0.3.0-SNAPSHOT"]
-                 [lamina "0.5.0-SNAPSHOT"]
+                 [aleph "0.3.0-rc1"]
+                 [lamina "0.5.0-rc2"]
                  [compojure "1.1.1"]
                  [ring-middleware-format "0.2.4" :exclusions [ring]]]
   :jvm-opts ["-Xmx4g"]
